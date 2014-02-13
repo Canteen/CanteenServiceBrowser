029a5fca8ea84e8651166a33df3adc3639e959b1
Matt Karl <matt@cloudkid.com>
2014-02-13 14:04:07 -0500

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ServiceBrowser",
+        "URIUtils"
+    ],
+    "modules": [
+        "Canteen_ServiceBrowser"
+    ],
+    "allModules": [
+        {
+            "displayName": "Canteen\\ServiceBrowser",
+            "name": "Canteen_ServiceBrowser"
+        }
+    ]
+} };
+});
